ce5183c1a8b64e56cbbdd4636de46305e79307cf
kvasquez <kvasquez@k-lagan.com>
2026-02-15 09:24:56 +0100

    singlepage working

---
+++ src/main/java/com/example/demo/HolamundoController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HolamundoController {
+
+    @GetMapping("/saludo")
+    public String saludar(Model model) {
+        // Pasamos datos al HTML usando el objeto Model
+        model.addAttribute("mensaje", "¡Hola desde Spring Boot con Thymeleaf!");
+        model.addAttribute("fecha", new java.util.Date().toString());
+        
+        // Retornamos el nombre del archivo HTML (sin el .html)
+        return "hola"; 
+    }
+}
